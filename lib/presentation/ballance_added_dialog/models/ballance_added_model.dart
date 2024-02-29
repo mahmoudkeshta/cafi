d8694c215e77d8ec113f6adbf049e0847105beab
mahmoudkeshta <mahmoudkeshta1@gmail.com>
2024-02-29 20:55:26 +0400

    classs used

--- lib/presentation/ballance_added_dialog/models/ballance_added_model.dart
+++ lib/presentation/ballance_added_dialog/models/ballance_added_model.dart
@@ -1,3 +1,23 @@
 /// This class defines the variables used in the [ballance_added_dialog],
-/// and is typically used to hold data that is passed between different parts of the application.
-class BallanceAddedModel { }
+/// and is typica passed between different parts of the app?
+
+class User {
+String? name;
+ String? email;
+String? about;
+
+String? image;
+
+
+String? createdAt;
+
+String? lastActivated;
+bool? online;
+User({
+    required this. id, required this. name, required this.email, required this. about, required this. image, required this. createdAt, required this. lastActivated, required this.puchToken, required this.online,
+});
+
+
+
+}
+
--- lib/presentation/sign_in_screen/models/sign_in_model.dart
+++ lib/presentation/sign_in_screen/models/sign_in_model.dart
@@ -1,3 +1,73 @@
 /// This class defines the variables used in the [sign_in_screen],
 /// and is typically used to hold data that is passed between different parts of the application.
-class SignInModel { }
+class used { 
+String? id ;
+String? id;
+
+String?
+name;
+String?
+email;
+String? about;
+
+String? image;
+
+
+String? createdAt;
+
+String? lastActivated;
+
+String? puchToken;
+
+
+目
+File
+Edit
+Selection
+View
+Go
+Run
+Terminal
+Help
+→
+• chat_material3
+EXPLORER
+creen.dart
+contact_home_screen.dart
+contact_card.dart
+> OPEN EDITORS
+V CHAT MATERIAL3
+dart_tool idea android assets
+build
+ios ios
+lib
+models user_model.dart
+screens
+e utils
+# widgets
+firebase_options.dart
+layout.dart main.dart
+linux
+lib > models >
+user_model.dart › €, ChatUser ›
+class Chatuser {
+ChatUser
+ macos
+test
+web
+= windows
+flutter-plugins flutter-plugins-dependencies gitignore
+-metadata analysis_options.yaml chat material3.iml
+› PROBLEMS
+> OUTLINE
+› TIMELINE
+> DEPENDENCIES
+> JAVA PROJECTS
+@1 Д 10 157
+Debug my code
+bool? online;
+chatUser ({
+required this. id, required this. name, required this.email, required this. about, required this. image, required this. createdAt, required this. lastActivated, required this.puchToken, required this.online,
+}):
+
+}
