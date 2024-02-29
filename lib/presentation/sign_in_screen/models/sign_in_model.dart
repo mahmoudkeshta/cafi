afdab0d56250df67e4046239a663fcadc233ed80
mahmoudkeshta <mahmoudkeshta1@gmail.com>
2024-03-01 01:12:43 +0400

    updata class

--- lib/presentation/sign_in_screen/models/sign_in_model.dart
+++ lib/presentation/sign_in_screen/models/sign_in_model.dart
@@ -20,52 +20,6 @@ String? lastActivated;
 String? puchToken;
 
 
-目
-File
-Edit
-Selection
-View
-Go
-Run
-Terminal
-Help
-→
-• chat_material3
-EXPLORER
-creen.dart
-contact_home_screen.dart
-contact_card.dart
-> OPEN EDITORS
-V CHAT MATERIAL3
-dart_tool idea android assets
-build
-ios ios
-lib
-models user_model.dart
-screens
-e utils
-# widgets
-firebase_options.dart
-layout.dart main.dart
-linux
-lib > models >
-user_model.dart › €, ChatUser ›
-class Chatuser {
-ChatUser
- macos
-test
-web
-= windows
-flutter-plugins flutter-plugins-dependencies gitignore
--metadata analysis_options.yaml chat material3.iml
-› PROBLEMS
-> OUTLINE
-› TIMELINE
-> DEPENDENCIES
-> JAVA PROJECTS
-@1 Д 10 157
-Debug my code
-bool? online;
 chatUser ({
 required this. id, required this. name, required this.email, required this. about, required this. image, required this. createdAt, required this. lastActivated, required this.puchToken, required this.online,
 }):
