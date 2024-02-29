686a305090a41eedeb1f4a92b4e4ca2c2e32983d
mahmoudkeshta <mahmoudkeshta1@gmail.com>
2024-03-01 01:40:08 +0400

    SignInModel

--- lib/presentation/sign_in_screen/models/sign_in_model.dart
+++ lib/presentation/sign_in_screen/models/sign_in_model.dart
@@ -1,6 +1,6 @@
 /// This class defines the variables used in the 
 /// and is typically used to hold data that is passed between different parts of the application.
-class used { 
+class SignInModel{ 
 String? id ;
 
 
@@ -21,7 +21,7 @@ String? puchToken;
 
 String? online;
 
-chatUser ({
+SignInModel({
 required this. id, required this. name, required this.email, required this. about, required this. image, required this. createdAt, required this. lastActivated, required this.puchToken, required this.online,
 }):
 
