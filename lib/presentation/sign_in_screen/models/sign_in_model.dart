60f50ded09fa0146ae17fbcf40b42248b882523e
mahmoudkeshta <mahmoudkeshta1@gmail.com>
2024-03-01 14:33:54 +0400

    i

--- lib/presentation/sign_in_screen/models/sign_in_model.dart
+++ lib/presentation/sign_in_screen/models/sign_in_model.dart
@@ -2,27 +2,25 @@
 /// and is typically used to hold data that is passed between different parts of the application.
 class SignInModel{ 
 String? id ;
-
-
-String?
-name;
-String?
-email;
-String? about;
-
+String?name;
+String?email;
+String?about;
 String? image;
-
-
 String? createdAt;
-
 String? lastActivated;
-
 String? puchToken;
-
 String? online;
 
 SignInModel({
-required this.id, required this. name, required this.email, required this.about, required this.image, required this.createdAt, required this.lastActivated, required this.puchToken, required this.online,
+required this.id,
+required this. name,
+ required this.email,
+  required this.about, 
+  required this.image,
+   required this.createdAt, 
+   required this.lastActivated, 
+   required this.puchToken, 
+   required this.online
 });
 
 }
